5421d010213a2e4c40ab9037fdeb800fc5cf37f4
talha chaudhary <talhaxh88@gmail.com>
2026-07-08 09:25:21 +0500

    connect with database

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+    );
+    console.log(
+      `
+      <========================> db is connected <==========================>`,
+      connectionInstance.connection.host,
+      connectionInstance.connection.name,
+    );
+  } catch (error) {
+    console.log("MONGODB connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
